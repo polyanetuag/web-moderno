7c40df05e89d92f6ab3e202aa75e06d218bfc815
Polyane Tuag <pollyportugues@hotmail.com>
2021-11-13 11:51:43 -0300

    insert mayors

---
+++ banco-relacional/inserirPrefeitos.sql
@@ -0,0 +1,15 @@
+select * from cidades
+
+insert into prefeitos 
+    (nome, cidade_id)
+VALUES
+    ('Rodrigo Neves', 2)
+    ('Raquel Lyra', 3)
+    ('Zenaldo Coutinho', NULL)
+
+select * from prefeitos
+
+insert into prefeitos
+    (nome, cidade_id)
+VALUES
+    ('Rafael Greca', NULL)
