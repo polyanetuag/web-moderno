1a17414893fd4772a291673308d4453f0e83df89
Polyane Tuag <pollyportugues@hotmail.com>
2021-11-13 11:12:43 -0300

    insert data with ID

---
+++ banco-relacional/inserirNovosEstados.sql
@@ -0,0 +1,9 @@
+insert into estados (id, nome, sigla, regiao, populacao)
+VALUES (1000, 'Novo', 'NV', 'Sul', 2.54)
+
+insert into estados (nome, sigla, regiao, populacao)
+VALUES ('Mais Novo', 'MN', 'Norte', 2.51)
+
+select *from estados
+
+-- pega o ultimo id e complementa a partir dele
